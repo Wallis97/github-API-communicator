c63c42e0fa36612f3cdc26a8681757939299bb33
Wallis97 <adamwallis97@gmail.com>
2023-12-09 13:52:53 +0100

    Changed to GitLab, created venv

---
+++ python/script-env/main.py
@@ -0,0 +1,12 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='GitLab API Script',
+        description='A simple script for retrieving information from GitLab via GitLab API')
+    
+    ## Define scripts arguments
+    parser.add_argument 
+
+if __name__==main:
+    main()
